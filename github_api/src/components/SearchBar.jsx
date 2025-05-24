6eab154f57530bb3075c1d722bab40d8981cd3b6
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-24 16:29:14 +0200

    test request to the user's endpoint

--- github_api/src/components/SearchBar.jsx
+++ github_api/src/components/SearchBar.jsx
@@ -4,10 +4,13 @@ export default function SearchBar() {
     const [data, setData] = useState([]);
     const [searchText, setSearchText] = useState("");
 
+    //"repository_search_url": "https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}"
+    //"user_organizations_url": "https://api.github.com/user/orgs"
+
     function handleFormSubmit(e) {
         e.preventDefault();
 
-        fetch(`https://api.github.com/search/repositories?q=${searchText}`, {
+        fetch(`https://api.github.com/search/users?q=${searchText}`, {
             headers: {
                 "Authorization": `Bearer ${import.meta.env.VITE_API_TOKEN}`,
                 "Accept": "application/vnd.github.v3+json",
@@ -16,6 +19,11 @@ export default function SearchBar() {
         })
             .then(response => response.json())
             .then(response => {
+                /*if (searchText.length <= 3) {
+                    alert("Numero di caratteri insufficienti");
+                } else {
+                    setData(response.items);
+                }*/
                 setData(response.items);
             })
             .catch((error) => console.error("Errore:", error));
@@ -33,15 +41,29 @@ export default function SearchBar() {
                     value={searchText}
                     onChange={(e) => setSearchText(e.target.value)}
                 />
+
+                <select id="options">
+                    <option value="">
+
+                    </option>
+                </select>
                 <button type="submit">Cerca</button>
             </form>
 
-            <ul>
+            {/*<ul>
                 {data && data.map(repo => (
                     <li key={repo.id}>
                         <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
                             {repo.full_name}
                         </a>
+                    </li>
+                ))}
+            </ul>*/}
+
+            <ul>
+                {data && data.map(user => (
+                    <li key={user.id}>
+                        {user.login}
                     </li>
                 ))}
             </ul>
